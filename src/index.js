049b9d5b070febf0163aad96d1a703f0a06283a0
Yuvaraj Tana <yuvaraj@tymtix.com>
2017-10-09 12:14:10 +0530

    Initial setup to start working with React, Webpack and ES6,7,8

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// let app_tech = 'ES6';
+// const msg = `Welcome to the ${app_tech} world`;
+// console.log("Hello World msg", msg);
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+ReactDOM.render(<p>Hello World</p>,document.getElementById('app'));
+
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const config = {
+	entry:'./src/index.js',
+	output:{
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js'
+	},
+	module:{
+		rules:[
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /(node_modules|bower_components)/,
+				use:{
+					loader:'babel-loader',
+					options: {
+					 	presets: ['env','react']
+					}
+				}
+			}
+		]
+	},
+	devServer:{
+		contentBase: path.join(__dirname, 'dist')
+	}
+};
+module.exports = config;
+
